d218569c39cb5e0fa46fe67bdafb97902375d259
Gerard <gerardmonso@gmail.com>
2024-03-12 19:31:43 +0100

    list class

--- src/main/scala/cat/udl/eps/is/List.scala
+++ src/main/scala/cat/udl/eps/is/List.scala
@@ -1,5 +1,7 @@
 package cat.udl.eps.is
 
+import scala.annotation.tailrec
+
 // Exercises in user defined lists
 
 enum List[+A] {
@@ -48,39 +50,152 @@ object List {
   // ------------------------------------------------------------------
 
   def partition[A](l: List[A])(p: A => Boolean): (List[A], List[A]) = {
-    ???
+    (foldLeft(reverse(l),  Nil: List[A]) { (acc, elem) =>
+      if p(elem) then Cons(elem, acc)
+      else acc
+    },
+      foldLeft(reverse(l), Nil: List[A]){ (acc, elem) =>
+        if !p(elem) then Cons(elem, acc)
+        else acc
+      })
   }
-  
+
+  // amb fold left
   def digitsToNum(l: List[Int]): Int = {
-    ???
+    foldLeft(l,""){ (acc, elem) =>
+      acc + elem
+    }.toInt
   }
-  
+
+  // de forma numerica i sense fold
+  def digitsToNum1(l: List[Int]): Int = {
+    def loop(acc: Int, l: List[Int], i: Int): Int = {
+      l match
+        case Nil => acc
+        case Cons(head, tail) => acc + (head * scala.math.pow(10, i)).toInt + loop(acc, tail, i+1)
+    }
+    loop(0, reverse(l), 0)
+  }
+
+  // sort una mica liat
+  def sort1[A](as: List[A])(ord: (A, A) => Boolean): List[A] = {
+    @tailrec
+    def loop(l: List[A], acc: List [A], mainHead: A|Boolean): List[A] = {
+      mainHead match
+        case false =>
+          l match
+            case Cons(head1: A, Cons(head2: A, tail2)) =>
+              if ord(head1, head2) then
+                loop(Cons(head2, tail2), append(acc, Cons(head1, Nil)), false)
+              else
+                loop(tail2, append(acc, Cons(head2, Nil)), head1)
+            case Cons(head: A, tail) =>
+              loop(tail, append(acc, Cons(head, Nil)), false)
+            case _ => acc
+        case mainHead:A =>
+          l match
+            case Cons(head: A, tail) =>
+              if ord( mainHead, head) then
+                loop(tail, append(acc, Cons(mainHead, Nil)), head)
+              else
+                loop(tail, append(acc, Cons(head, Nil)), mainHead)
+            case Nil => append(acc, Cons(mainHead, Nil))
+        case _ => acc
+    }
+    loop(as, Nil, false)
+  }
+
+  // el sort definitiu
   def sort[A](as: List[A])(ord: (A, A) => Boolean): List[A] = {
-    ???
+    @tailrec
+    def loop(l: List[A], acc: List[A]): List[A] = {
+      l match
+        case Cons(head1: A, Cons(head2: A, tail2)) =>
+          if ord(head1, head2) then
+            loop(Cons(head2, tail2), append(acc, Cons(head1, Nil)))
+          else
+            loop(Cons(head1, tail2), append(acc, Cons(head2, Nil)))
+        case Cons(head: A, Nil) =>
+          append(acc, Cons(head, Nil))
+        case _ => acc
+    }
+    loop(as, Nil)
   }
-  
+
+  /*def sort[A](as: List[A])(ord: (A, A) => Boolean): List[A] = {
+    def loop(l: List[A], acc: List [A], mainHead: A|Boolean): List[A] = {
+      mainHead match
+        case Nil => {
+          l match
+            case Cons(head1: A, Cons(head2: A, tail2)) =>
+              if ord(head1, head2) then
+                append(acc, Cons(head1, Nil))
+              else
+                Cons(head2, Cons(head1, tail2))
+        }
+        case Cons(head,tail) => ???
+    }
+    loop(as, (), false)
+  }*/
+
+  /*def sort1[A](as: List[A])(ord: (A, A) => Boolean): List[A] = {
+    foldLeft(as, ()){ (acc, elem) =>
+      elem match
+        //case Cons(head, Nil) => Cons(head, Nil)
+        case Cons(head1: A, Cons(head2: A, tail2)) =>
+          if ord(head1, head2) then
+            append(acc, Cons(head1, tail1))
+          else
+            append(acc, Cons(head1, tail1))
+
+    }
+  }*/
+
   def mergeSorted[A](l1: List[A], l2: List[A])(
-      ord: (A, A) => Boolean
+    ord: (A, A) => Boolean
   ): List[A] = {
-    ???
+    @tailrec
+    def loop(l1: List[A], l2: List[A], acc: List[A]): List[A] =
+      (l1,l2) match
+        case (Cons(head1, tail1), Cons(head2, tail2)) =>
+          if ord(head1, head2) then
+            loop(tail1, tail2, append(acc, Cons(head1, Cons(head2, Nil))) )
+          else
+            loop(tail1, tail2, append(acc, Cons(head2, Cons(head1, Nil))) )
+        case (Nil, Cons(head2, tail2)) => loop(Nil, tail2, append(acc, Cons(head2, Nil)))
+        case (Cons(head1, tail1), Nil) => loop(tail1, Nil, append(acc, Cons(head1, Nil)))
+        case (Nil, Nil) => acc
+    loop(l1, l2, List(): List[A])
   }
 
   def checkSorted[A](as: List[A])(ord: (A, A) => Boolean): Boolean = {
     ???
   }
 
+  @tailrec
   def find[A](l: List[A])(p: A => Boolean): Option[A] = {
-    ???
+    l match
+      case Nil => None
+      case Cons(head: A, tail: List[A]) =>
+        if p(head) then
+          Some(head)
+        else find(tail)(p)
   }
 
   def partitionMap[A, B, C](
-      l: List[A]
-  )(p: A => Either[B, C]): (List[B], List[C]) = {
+                             l: List[A]
+                           )(p: A => Either[B, C]): (List[B], List[C]) = {
     ???
   }
 
   def digitsToNumOption(l: List[Int]): Option[Int] = {
-    ???
+    foldLeft(l, Some(0)){(acc, elem) =>
+      if (acc.getOrElse(None) == None) then return None
+      if elem.toString.length > 1 then
+        return None
+      else
+        Some( acc.getOrElse(None).toString.concat(elem.toString).toInt)
+    }
   }
 
   def digitsToNumEither(l: List[Int]): Either[Int, Int] = {
--- src/test/scala/cat/udl/eps/is/ListSuite.scala
+++ src/test/scala/cat/udl/eps/is/ListSuite.scala
@@ -10,32 +10,32 @@ class ListSuite extends FunSuite {
     val result = Cons(3, Cons(2, Cons(1, Nil)))
     assertEquals(sort(l)(_ >= _), result)
   }
-  
+
   test("partition") {
     val l = List(11, 22, 33, 44, 55)
     val (odds, evens) = partition(l)(_ % 2 == 1)
     assertEquals(odds, List(11, 33, 55))
     assertEquals(evens, List(22, 44))
   }
-  
+
   test("digitsToNum") {
     val l = List(1,2,3,4)
     assertEquals(digitsToNum(l), 1234)
   }
-  
+
   test("sort") {
     val l = List(10, 7, 2, 3, 1, 4, 6, 5, 3, 2)
     assertEquals(sort(l)(_ <= _), List(1, 2, 2, 3, 3, 4, 5, 6, 7, 10))
     assertEquals(sort(l)(_ >= _), List(10, 7, 6, 5, 4, 3, 3, 2, 2, 1))
   }
-  
+
   test("checkSorted") {
     val sorted = List(1, 2, 3, 4, 5)
     val unsorted = List(1, 2, 4, 3, 5)
     assert(checkSorted(sorted)(_ <= _))
     assert(!checkSorted(unsorted)(_ <= _))
   }
-  
+
   test("digitsToNumOption") {
     val num = List(1, 2, 3, 4)
     val noNum = List(1, 20, 3, 4)
@@ -49,4 +49,23 @@ class ListSuite extends FunSuite {
     assertEquals(digitsToNumEither(num), Right(1234))
     assertEquals(digitsToNumEither(noNum), Left(20))
   }
+
+  test("find") {
+    val l = List(1, 2, 3, 4, 5)
+    assertEquals(find(l)(_ % 2 == 0), Some(2)) // 2 is the first even number in the list
+    assertEquals(find(l)(_ > 5), None) // there is no element greater than 5 in the list
+  }
+
+
+  test("mergeSorted") {
+    val l1 = List(1, 3, 5)
+    val l2 = List(2, 4, 6)
+    val l2_extended = List(2, 4, 6, 7, 8,9,10,11)
+    assertEquals(mergeSorted(l1, Nil)(_ <= _), List(1,3,5))
+    assertEquals(mergeSorted(Nil, l2)(_ <= _), List(2,4,6))
+    assertEquals(mergeSorted(Nil, l2)(_ <= _), List(2,4,6))
+    assertEquals(mergeSorted(l1, l2)(_ <= _), List(1, 2, 3, 4, 5, 6))
+    assertEquals(mergeSorted(l1, l2_extended)(_ <= _), List(1,2,3,4,5,6,7,8,9,10,11))
+  }
+
 }
